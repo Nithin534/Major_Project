217164b573f2003f1be08361d1a7127d34df1560
Nithin <nithinakuthota25@gmail.com>
2024-09-20 18:39:19 +0530

    Major Files

---
+++ run.bat
@@ -0,0 +1,17 @@
+1.Concept-
+     Abstract, Exsiting, Proposed, Modules
+2.Developemnt-
+   Python, Html,Css.Js, Flask.
+   VS code-IDE
+3. Algorithms- ANN- 
+4. Libraies.
+5. Result
+6. Conclusion and Fucture scope.
+========================================
+
+1.Dataset --where --audios-- GPU.
+2. Prepocessing
+3. TRain
+4. Test
+5. ANN Algorithm
+6. Result--web application--
